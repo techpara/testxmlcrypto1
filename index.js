9ec0bc08cc794e36d659b5d27bc04fa92f0550a1
Pritesh TechArk <pritesh.techark@gmail.com>
2020-08-07 13:15:16 +0000

    Updated certificate

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ var fs = require('fs');
 var path = require('path');
 var xml_crypto_1 = require("xml-crypto");
 var dom = require('xmldom').DOMParser
-	var x509Certificate="MIIDbTCCAlSgAwIBAgIBADANBgkqhkiG9w0BAQsFADBQMQswCQYDVQQGEwJ1czELMAkGA1UECAwCVkExFzAVBgNVBAoMDnJlY3ljbGluZ3BlcmtzMRswGQYDVQQDDBJyZWN5Y2xpbmdwZXJrcy5jb20wHhcNMjAwODA1MTUzNTIyWhcNMjEwODA1MTUzNTIyWjBQMQswCQYDVQQGEwJ1czELMAkGA1UECAwCVkExFzAVBgNVBAoMDnJlY3ljbGluZ3BlcmtzMRswGQYDVQQDDBJyZWN5Y2xpbmdwZXJrcy5jb20wggEjMA0GCSqGSIb3DQEBAQUAA4IBEAAwggELAoIBAgC3cIS2Zi60PSIjSaHr1y/2C+F7LWOVIEUAcmBgbYvFdMYU/hOBo4WH/C3iPHPjRx2NAYZiJnMEy/1AQFacYpyknGTpIl8gb43CLM1MJ5lgICPANPq9qA5XkO8KY+IZcp4sL4mL9i5o7QcJd/AV7E1XbrehCvLU5q66CEAQvcgt8qZMbG3lHoOR+3mq82Zetr8IrlbVWIqbuAGJbIcdpGZ/7kkDOivE9ASFTWfmFviW4pjIVQUbM05tmaasad4IGnT1y8uxPcCHETuepSeu4CPLZNg0ZHiSKwpEk3ZlGVVOYY+Lu/R6VfKH2E/JSveE6FxUFRDkQUbNOX8UGy9eKlKqgQIDAQABo1AwTjAdBgNVHQ4EFgQU4JM8SlZLzJYhS84jtinF+PHHSrIwHwYDVR0jBBgwFoAU4JM8SlZLzJYhS84jtinF+PHHSrIwDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQsFAAOCAQIAsfovQVrY44p3enoQyf3+kg+kLrbgXm1tM8nU9JMVE26SgZCXak1EIUBP0Y0MLTQharynkTNjyQ8jVXgKIS1AuCMy/XpQoixXh0b+kpi0h3jmHH7WvvFkQCD5manWn1UGfEyGNHEM684aDw/Zp6WPUXiYCgZAYZ7gZRZInAoNTYP3p+iqnqZ7KKitvEGb/vcYueYO50rsORyCipAtLIFBoczfkqWWVbm3iUhFTbsIaHFE3copHf1+PxiUYsNyCJJU407P+QRpthAB7j9PhWddW3CI9HwzGUpHW/2JVKlmjfB9xyFYhaNJsfFMZYCAaY6lcWW5MvukaTosst81PZMdWE4=";
+	var x509Certificate="MIIEGTCCAwGgAwIBAgIUAnE9HtD9wEL5aKZ2Bo2lK2REvugwDQYJKoZIhvcNAQELBQAwgZsxCzAJBgNVBAYTAlVTMQswCQYDVQQIDAJWQTEQMA4GA1UEBwwHTm9yZm9sazEXMBUGA1UECgwOUmVjeWNsaW5nUGVya3MxCzAJBgNVBAsMAklUMR8wHQYDVQQDDBZhcHAucmVjeWNsaW5ncGVya3MuY29tMSYwJAYJKoZIhvcNAQkBFhdpbmZvQHJlY3ljbGluZ3BlcmtzLmNvbTAeFw0yMDA4MDYxMTExMjhaFw0yMzA1MDMxMTExMjhaMIGbMQswCQYDVQQGEwJVUzELMAkGA1UECAwCVkExEDAOBgNVBAcMB05vcmZvbGsxFzAVBgNVBAoMDlJlY3ljbGluZ1BlcmtzMQswCQYDVQQLDAJJVDEfMB0GA1UEAwwWYXBwLnJlY3ljbGluZ3BlcmtzLmNvbTEmMCQGCSqGSIb3DQEJARYXaW5mb0ByZWN5Y2xpbmdwZXJrcy5jb20wggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQC4460mU0tYEJEns5gDGAtCNYR0M5QWPXqKh64Q9Nm8tOxCspKXBBttPeqb8U0RgnKeDO4zJiLOdi6dbYsdB9TzpJyf//ieywHihgDtLxbdN7Qy+lM0jiPpx9bh/PJi3FwOHR3ZdsSxl7p0dKGXWhyyjztB4tIG6VjFg2h9EtrV47ZLGSsU1q26h6wMgWaQkZ5kAssFlzjFgudAH+ggpihmxaBJvvDB2A9fJSagE/Oq4bL7uqD+FOFbK8ngtXe8rzuXzLI/Ivbprg7/vUwNNZpjaQa7CtrtOk2vsRmMB1FnGOu8iF/sSld/3v24v7sOT/BHnRWa3RJObiQFzccMPGPrAgMBAAGjUzBRMB0GA1UdDgQWBBS/7+XQwMhtxM+rgEh/9w5yQLkyijAfBgNVHSMEGDAWgBS/7+XQwMhtxM+rgEh/9w5yQLkyijAPBgNVHRMBAf8EBTADAQH/MA0GCSqGSIb3DQEBCwUAA4IBAQA87Nbv3dv/gR9mqtGVfZ3JyS4o2vGmuHWvn8hAcBxALuwxwmXbpjJMdmVOm3nG25TpRsibmINUJ4Y1tCLdTjY6YAyeESkjjNAi+38Xyv2tRWs+9iE7huvK/1FVPK1DY0grB3TEGaLL8SQBwUl/RU4/XdiM9QY5bD59V5Ae+boF4Ym18enek5FzEyFsnO3zONsMVqGOh0weVldozGIOQlEr3wuqT+PyhTzxmIXNXAN4cUFs6fQWAChGL/43yItX3CpzxvM89GSScrzMV95R6awf1XK55R4/sTbfNajsjszQUi3yV/HyWOb2qInyqewjKHUAWIFosYN6DYZ5lq3aX74M";
 	var SignedXml = require('xml-crypto').SignedXml	  ;
 	var xml =fs.readFileSync('./security/unsigned-sample-response.xml', 'utf-8');
 	var sig = new SignedXml()
-	sig.signingKey =  fs.readFileSync('./security/private-key.pem');
+	sig.signingKey =  fs.readFileSync('./security/RP-2048-RSA256-PrivateKey.pem');
 	sig.keyInfoProvider = new MyKeyInfo(x509Certificate);
   sig.canonicalizationAlgorithm = "http://www.w3.org/2001/10/xml-exc-c14n#";
   sig.signatureAlgorithm = "http://www.w3.org/2001/04/xmldsig-more#rsa-sha256";
@@ -28,7 +28,7 @@ var dom = require('xmldom').DOMParser
 	var xml = fs.readFileSync("./security/signed-sample-sml.xml").toString()
 	var doc = new dom().parseFromString(xml) ;  
 	var signature = select(doc, "//*[local-name(.)='Signature' and namespace-uri(.)='http://www.w3.org/2000/09/xmldsig#']")[0];
-	sig.keyInfoProvider = new FileKeyInfo('./security/RP-X509.cer');
+	sig.keyInfoProvider = new FileKeyInfo('./security/RP-X509-RSA256-2048BIT.cer');
 	sig.loadSignature(signature.toString());
 	var res = sig.checkSignature(xml);
 	if (!res)
